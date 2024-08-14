3891186d88e76d18ec8be15da4e18748b67fd2ac
andrei <andreimendes8@gmail.com>
2024-08-14 09:32:31 -0300

    condicionais 2

--- main.py
+++ main.py
@@ -30,13 +30,26 @@
 # print("divisao", divisao)
 # print("potencia", potencia)
 
-# condicionais
+# condicionais 1
 
-idade = int( input("Informe a sua idade: ") )
+# idade = int( input("Informe a sua idade: ") )
 
-if idade >= 18:
-    print("PERMITIDO!")
-else:
-    print("NEGADO!")
+# if idade >= 18:
+#     print("PERMITIDO!")
+# else:
+#     print("NEGADO!")
+
+# condicionais 2
+
+salario = float(input("Informe seu salário: "))
+
+if salario <= 3000:
+    print("programador junior")
+elif salario > 3000 and salario <=6000:
+    print("programador pleno")
+elif salario > 6000 and salario <=15000:
+    print("programador senior")
+else: 
+    ("gerente de projetos")
 
 
