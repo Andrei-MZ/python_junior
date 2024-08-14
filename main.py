f91ec20a45226c71bf222e9f209432b3feb89ab2
andrei <andreimendes8@gmail.com>
2024-08-14 09:20:01 -0300

    iniciando python

---
+++ main.py
@@ -0,0 +1,26 @@
+# variaveis python
+
+# nome = "Andrei"
+# idade = 27
+# peso = 71.3
+
+# print(nome)
+# print(idade)
+# print(peso)
+
+# #  input python 
+
+# nome = input("Digite seu nome:")
+# idade = int( input("Digite sua idade:") )
+# peso = float( input("Digite seu peso:") )
+
+# print(nome)
+# print(type(idade))
+# print(type(peso))
+
+# operadores
+
+soma = 1 + 1
+multiplicacao = 4 * 4 
+divisao = 30 / 3
+potencia = 7 ** 2
