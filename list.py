cd538e45493d56039e220d1f86713f65ff48dcbb
andrei <andreimendes8@gmail.com>
2024-08-14 09:45:09 -0300

    listas

--- list.py
+++ list.py
@@ -1,1 +1,18 @@
 # Listas
+
+lista_numeros = [1, 2, 3]
+
+print(lista_numeros[0])
+print(lista_numeros[1])
+print(lista_numeros[2])
+
+# alterando o resultado do index "0"
+
+lista_numeros = [1, 2, 3]
+
+lista_numeros[0] = 5
+
+print(lista_numeros[0])
+print(lista_numeros[1])
+print(lista_numeros[2])
+
